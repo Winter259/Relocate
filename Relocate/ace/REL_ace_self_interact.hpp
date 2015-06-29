15b3c10a54c72956557a934a888e04258737b991
Simon AM <simon@agius-muscat.net>
2015-06-29 19:02:45 +0200

    Pasted example

--- Relocate/ace/REL_ace_self_interact.hpp
+++ Relocate/ace/REL_ace_self_interact.hpp
@@ -1,1 +1,14 @@
-// Placeholder
+class ACE_SelfActions {
+    class ACE_Relocate {
+        class ACE_REL_Deploy {
+            //displayName = CSTRING(AttachDetach);
+            //condition = QUOTE(_this call FUNC(canAttach));
+            //insertChildren = QUOTE(_this call FUNC(getChildrenAttachActions));
+            exceptions[] = {"isNotDragging","isNotSitting","notOnMap"};
+            showDisabled = 0;
+            priority = 5;
+            //icon = ""; // TODO
+            // hotkey = "D"; 
+        };
+    };
+};
