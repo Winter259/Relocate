436be6b6cc41cc304eb4287fdc6805a05cccf0d5
Simon AM <simon@agius-muscat.net>
2015-06-30 00:23:36 +0200

    Attempting fix

--- Relocate/ace/REL_ACE_Deploy.hpp
+++ Relocate/ace/REL_ACE_Deploy.hpp
@@ -1,15 +1,17 @@
-class Man;
-class CAManBase: Man {
-    class ACE_SelfActions {
-        class ACE_REL_Deploy {
-            displayName = "Deploy Group";
-            condition = "true";
-            exceptions[] = {"isNotDragging","isNotSitting","notOnMap"};
-            statement = "";
-            showDisabled = 1;
-            priority = 5;
-            icon = "Relocate/ace/deploy.paa";
-            hotkey = "D";
+class CfgVehicles {
+    class Man;
+    class CAManBase: Man {
+        class ACE_SelfActions {
+            class ACE_REL_Deploy {
+                displayName = "Deploy Group";
+                condition = "true";
+                exceptions[] = {"isNotDragging","isNotSitting","notOnMap"};
+                statement = "";
+                showDisabled = 0;
+                priority = 5;
+                icon = "Relocate/ace/deploy.paa";
+                hotkey = "D";
+            };
         };
     };
 };
